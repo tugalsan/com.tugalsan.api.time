1a632487038570eff2d3d5efaaa43017248513bc
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-27 12:08:01 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,7 @@ module com.tugalsan.api.time {
     requires com.tugalsan.api.unsafe;
     requires com.tugalsan.api.union;
     requires com.tugalsan.api.charset;
-    requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.function;
     requires com.tugalsan.api.string;
     exports com.tugalsan.api.time.client;
     exports com.tugalsan.api.time.server;
